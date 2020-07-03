0986d5471342bfc8dd26f52ef238de2d06ff0c2f
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-07-03 19:07:18 +0530

    ListNode

---
+++ Leet-code 30 Days Challenge/Week_02/ListNode.java
@@ -0,0 +1,11 @@
+package Week_02;
+
+public class ListNode {
+	 
+		 int val;
+		 ListNode next;
+		 ListNode() {}
+		      ListNode(int val) { this.val = val; }
+		      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	 
+}
